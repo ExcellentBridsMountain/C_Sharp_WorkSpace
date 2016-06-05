225c16269cb3b50c5ca727c9a2392db75e82cb3a
Woody-Hu <812791840@qq.com>
2016-06-05 13:08:11 +0800

    2016 06 06 Thread
    not complete

---
+++ ThreadSamples/SampleOne/ThreadWineFrom/ThreadWineFrom/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ThreadWineFrom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_toZero = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(12, 130);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(78, 35);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(128, 131);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(85, 32);
+            this.button_stop.TabIndex = 2;
+            this.button_stop.Text = "Stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(123, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // button_toZero
+            // 
+            this.button_toZero.Location = new System.Drawing.Point(87, 200);
+            this.button_toZero.Name = "button_toZero";
+            this.button_toZero.Size = new System.Drawing.Size(75, 23);
+            this.button_toZero.TabIndex = 4;
+            this.button_toZero.Text = "To ZERO";
+            this.button_toZero.UseVisualStyleBackColor = true;
+            this.button_toZero.Click += new System.EventHandler(this.button_toZero_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button_toZero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_toZero;
+
+    }
+}
+
